4eca271716e2d8470859747929d35d7b1e1d27be
ProphetHJK <prophethjk@gmail.com>
2021-12-16 17:04:12 +0800

    first commit

---
+++ main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.kawabangga.com/posts/4194
+// @icon         https://www.google.com/s2/favicons?domain=kawabangga.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
